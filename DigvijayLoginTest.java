dc2d50de9b7c8be5a417550173bf4545d1683ded
dpsirit189 <71250438+dpsirit189@users.noreply.github.com>
2020-09-25 11:59:18 +0530

    Update DigvijayLoginTest.java

--- DigvijayLoginTest.java
+++ DigvijayLoginTest.java
@@ -9,6 +9,7 @@ import org.testng.annotations.Test;
 
 public class DigvijayLoginTest {
 	// my code
+	//developer 2 added this
   @Test(dataProvider="dp")
   public void login(String us,String pw) {
 	  System.setProperty("webdriver.chrome.driver", "D:\\data\\st\\chromedriver.exe");  // path of chrome driver
